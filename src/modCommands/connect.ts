a1fa1e9d4c369dadd4ce1436014ad50d17f6e8fa
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-07-18 11:17:02 -0700

    Removing my username in command descriptions and replacing with a more generic term

--- src/modCommands/connect.ts
+++ src/modCommands/connect.ts
@@ -4,11 +4,11 @@ import { connectToDB, registerCommands } from '../bot'
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('connect')
-		.setDescription("Refresh Europa's connection to the database (reserved for Cryo)")
+		.setDescription("Refresh Europa's connection to the database (Developer Only)")
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
 		if (interaction.user.id !== '251458435554607114') {
-			return interaction.reply('This command is reserved for Cryo.')
+			return interaction.reply({content: 'You do not have permission to use this command.', ephemeral: true})
 		}
 		
 		await interaction.reply('Connecting to Database <a:loading:763160594974244874>')
--- src/modCommands/respawn.ts
+++ src/modCommands/respawn.ts
@@ -4,12 +4,12 @@ import { client } from '../bot'
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('respawn')
-		.setDescription('Force a Europa shard to respawn')
+		.setDescription('Force a Europa shard to respawn (Developer Only)')
 		.addNumberOption(option => option.setName('number').setDescription('The number for the shard to respawn').setRequired(true))
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
 		if (interaction.user.id !== '251458435554607114') {
-			return interaction.reply('This command is reserved for Cryo.')
+			return interaction.reply({content: 'You do not have permission to use this command.', ephemeral: true})
 		}
 		
 		const shardNumber = interaction.options.getNumber('number')!
