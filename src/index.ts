cdeea5279144f650cff7db47ef38929e24439409
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-07-18 11:13:32 -0700

    Simplifying the code for the sharding manager

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { ShardingManager } from 'discord.js'
 
-const manager = new ShardingManager('./prod/bot.js', {
-    token: process.env.BOT_TOKEN,
-    totalShards: 'auto'
-})
+const manager = new ShardingManager('./prod/bot.js', {token: process.env.BOT_TOKEN})
 
 manager.on('shardCreate', shard => {
     console.log(`[${new Date().toString().split(' ', 5).join(' ')}] Launched Shard #${shard.id}`)
 })
-manager.spawn({amount: manager.totalShards, delay: 10000, timeout: -1})
+
+manager.spawn({amount: 'auto', delay: 10000, timeout: -1})
