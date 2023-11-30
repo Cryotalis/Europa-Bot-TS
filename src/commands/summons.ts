fbae372e6d84e439b56e1469d7c30d1da61a0292
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-11-29 17:58:59 -0800

    Using a guard clause to improve the code

--- src/commands/summons.ts
+++ src/commands/summons.ts
@@ -31,52 +31,50 @@ module.exports = {
 
 		if (!data) return interaction.editReply('Unable to connect to database. Please try again in a few seconds.')
 
-		if (playerID){
-			loadSummons(interaction, playerID)
-		} else {
-			const searchEmbed = new EmbedBuilder()
-				.setTitle(`Searching for "${playerName}" <a:loading:763160594974244874>`)
-				.setColor('Blue')
-				.setAuthor({name: 'Player Search', iconURL: 'https://upload.wikimedia.org/wikipedia/en/e/e5/Granblue_Fantasy_logo.png'})
-				.setFooter({text: 'http://info.gbfteamraid.fun/web/about', iconURL: 'http://info.gbfteamraid.fun/view/image/icon.png'})
+		if (playerID) return loadSummons(interaction, playerID)
+		
+		const searchEmbed = new EmbedBuilder()
+			.setTitle(`Searching for "${playerName}" <a:loading:763160594974244874>`)
+			.setColor('Blue')
+			.setAuthor({name: 'Player Search', iconURL: 'https://upload.wikimedia.org/wikipedia/en/e/e5/Granblue_Fantasy_logo.png'})
+			.setFooter({text: 'http://info.gbfteamraid.fun/web/about', iconURL: 'http://info.gbfteamraid.fun/view/image/icon.png'})
 
-			await interaction.editReply({embeds: [searchEmbed]})
+		await interaction.editReply({embeds: [searchEmbed]})
 
-			interface player {level: string, name: string, userid: string}
-			const options = { // https://info.gbfteamraid.fun version
-				method: 'POST',
-				url: 'https://info.gbfteamraid.fun/web/userrank',
-				headers: {
-				  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-				  Cookie: `JSESSIONID=${jsessionID}`
-				},
-				data: {method: 'getUserrank', params: `{"username":"${playerName}"}`}
-			}
-			
-			let players = await axios.request(options).then(({data: {result}}) => result as player[]).catch(() => undefined) 
-			
-			if (!players){ // Fall back to https://gbfdata.com version if teamraid is not available
-				searchEmbed.setFooter({text: 'https://gbfdata.com/', iconURL: 'https://gbfdata.com/favicon.ico'})
-				interaction.editReply({embeds: [searchEmbed]})
-				players = await axios.get(`https://gbfdata.com/user/search?q=${urlencode(playerName)}&is_fulltext=1`).then(({data}) => {
-					const playerData = String(data.match(/(?<=\/thead>).+(?=<\/table)/s))
-					const playerMatches = playerData.matchAll(/href=".+?(\d+)">\n(.+?)\n<.+?"num">(\d+)/gs)
-					return [...playerMatches].map(player => ({userid: player[1], name: player[2], level: player[3]}))
-				}).catch(() => undefined)
-			}
-
-			if (!players) return interaction.editReply({content: 'Player search is currently unavailable. Please try again later.', embeds: []})
-			if (!players.length) return interaction.editReply({content: 'No players were found.', embeds: []})
-			if (players.length === 1) return loadSummons(interaction, players[0].userid)
-
-			players.sort((a, b) => parseInt(b.level) - parseInt(a.level))
-			players.sort((a, b) => compareTwoStrings(b.name, playerName) - compareTwoStrings(a.name, playerName))
-			const formattedPlayers = players.map(player => `${player.name} Rank ${player.level} (${player.userid})`)
-
-			const userChoice = await showMenu(interaction, playerName, formattedPlayers)
-			if (!userChoice) return
-			
-			loadSummons(interaction, userChoice.match(/(?<=\()\d+/)![0])
+		interface player {level: string, name: string, userid: string}
+		const options = { // https://info.gbfteamraid.fun version
+			method: 'POST',
+			url: 'https://info.gbfteamraid.fun/web/userrank',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+				Cookie: `JSESSIONID=${jsessionID}`
+			},
+			data: {method: 'getUserrank', params: `{"username":"${playerName}"}`}
 		}
+		
+		let players = await axios.request(options).then(({data: {result}}) => result as player[]).catch(() => undefined) 
+		
+		if (!players){ // Try searching https://gbfdata.com if teamraid is not available
+			searchEmbed.setFooter({text: 'https://gbfdata.com/', iconURL: 'https://gbfdata.com/favicon.ico'})
+			interaction.editReply({embeds: [searchEmbed]})
+			players = await axios.get(`https://gbfdata.com/user/search?q=${urlencode(playerName)}&is_fulltext=1`).then(({data}) => {
+				const playerData = String(data.match(/(?<=\/thead>).+(?=<\/table)/s))
+				const playerMatches = playerData.matchAll(/href=".+?(\d+)">\n(.+?)\n<.+?"num">(\d+)/gs)
+				return [...playerMatches].map(player => ({userid: player[1], name: player[2], level: player[3]}))
+			}).catch(() => undefined)
+		}
+
+		if (!players) return interaction.editReply({content: 'Player search is currently unavailable. Please try again later.', embeds: []})
+		if (!players.length) return interaction.editReply({content: 'No players were found.', embeds: []})
+		if (players.length === 1) return loadSummons(interaction, players[0].userid)
+
+		players.sort((a, b) => parseInt(b.level) - parseInt(a.level))
+		players.sort((a, b) => compareTwoStrings(b.name, playerName) - compareTwoStrings(a.name, playerName))
+		const formattedPlayers = players.map(player => `${player.name} Rank ${player.level} (${player.userid})`)
+
+		const userChoice = await showMenu(interaction, playerName, formattedPlayers)
+		if (!userChoice) return
+		
+		loadSummons(interaction, userChoice.match(/(?<=\()\d+/)![0])
 	}
 }
