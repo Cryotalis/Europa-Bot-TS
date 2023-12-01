df0e7e66d1f0784d486e4f12826e35fba0557cc7
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-12-01 14:49:11 -0800

    Improved role recognition and output message

--- src/commands/role.ts
+++ src/commands/role.ts
@@ -15,23 +15,26 @@ module.exports = {
 		const server = servers.find(server => server.guildID === interaction.guildId)
 		if (!server || !server.roles) return interaction.reply('No roles are set for your server.')
 		
-		const rolesInput = interaction.options.getString('roles')!.match(/[\w\s]+/g)?.filter(r => /\w/.test(r))
+		const rolesInput = interaction.options.getString('roles')!.replace(/(<@&\d+>)/g, '$1, ').match(/(?! )[^,]+(?<! )/g)
 		if (!rolesInput?.length) return interaction.reply('The roles you provided were invalid.')
 
 		const serverRolesConfig: categoryRole[] = JSON.parse(server.roles)
 		const serverRoles = interaction.guild?.roles.cache.filter(role => role.name !== '@everyone' && !role.managed)
+		if (!serverRoles) return interaction.reply('Your server does not have any roles that I can assign to you.')
+		
 		const member = interaction.member as GuildMember
-
-		const addedRoles: Role[] = [], notAddedRoles: (Role|string)[] = [], removedRoles: Role[] = []
+		const addedRoles: Role[] = [], removedRoles: Role[] = [], notAddedRoles: (Role|string)[] = [], notPermittedRoles: Role[] = []
 		rolesInput.forEach(roleInput => {
-			const role = /^\d+$/.test(roleInput)
-				? serverRoles?.find(role => role.id === roleInput)
-				: serverRoles?.find(role => role.name === findBestMatch(roleInput, serverRoles.map(role => role.name)).bestMatch.target)
+			const role = /<@&\d+>/.test(roleInput)
+				? serverRoles.find(role => role.id === roleInput.match(/\d+/)![0])
+				: serverRoles.find(role => role.name === findBestMatch(roleInput, serverRoles.map(role => role.name)).bestMatch.target)
 			
 			if (!role){
 				notAddedRoles.push(roleInput)
-			} else if (!serverRolesConfig.some(serverRole => serverRole.id === role.id) || clientUser.roles.highest.position <= role.position) {
+			} else if (!serverRolesConfig.some(serverRole => serverRole.id === role.id)) {
 				notAddedRoles.push(role)
+			} else if (clientUser.roles.highest.position <= role.position) {
+				notPermittedRoles.push(role)
 			} else if (member.roles.cache.find(r => r === role)){
 				member.roles.remove(role)
 				removedRoles.push(role)
@@ -42,15 +45,13 @@ module.exports = {
 		})
 		
 		const rolesEmbed = new EmbedBuilder()
-			.setAuthor({
-				name: `Roles were${addedRoles.concat(removedRoles).length === 0 ? ' not ' : ' '}changed for ${member.nickname ?? member.user.username}`,
-				iconURL: interaction.user.displayAvatarURL({extension: 'png'})
-			})
+			.setAuthor({name: `Roles changes for ${member.nickname ?? member.user.username}:`, iconURL: interaction.user.displayAvatarURL({extension: 'png'})})
 			.setColor('Blue')
 
-		if (addedRoles.length > 0) rolesEmbed.addFields([{name: 'Added Roles:', value: `${addedRoles.join(' ')}`}])
-		if (removedRoles.length > 0) rolesEmbed.addFields([{name: 'Removed Roles:', value: `${removedRoles.join(' ')}`}])
-		if (notAddedRoles.length > 0) rolesEmbed.addFields([{name: 'Unavailable Roles:', value: `${notAddedRoles.join(' ')}`}])
+		if (addedRoles.length) rolesEmbed.addFields([{name: 'Added Roles:', value: `${addedRoles.join(' ')}`}])
+		if (removedRoles.length) rolesEmbed.addFields([{name: 'Removed Roles:', value: `${removedRoles.join(' ')}`}])
+		if (notAddedRoles.length) rolesEmbed.addFields([{name: 'Roles not available on the server role list:', value: `${notAddedRoles.join(' ')}`}])
+		if (notPermittedRoles.length) rolesEmbed.addFields([{name: 'Roles I don\'t have permission to assign:', value: `${notAddedRoles.join(' ')}`}])
 
 		return interaction.reply({embeds: [rolesEmbed]})
 	}
