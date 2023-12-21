3c52d57d50382ce8d1e26ae5f92a008eba2de26d
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-12-20 19:08:58 -0800

    Simplifying the process of loading assets This makes adding new assets much easier than before

--- src/modules/assets.ts
+++ src/modules/assets.ts
@@ -1,5 +1,6 @@
 import { Image, loadImage } from 'canvas'
 import { currentShardID } from '../bot'
+import { readdirSync } from 'fs'
 
 export let playerTemplate: Image
 export let summonsTemplate: Image
@@ -35,81 +36,54 @@ export let progressBars: Image[]
 
 export let skydomWallpaper: Image
 export async function loadAssets(){
-    [
-        playerTemplate,
-        summonsTemplate,
-        privateSummon,
-        openSummon,
-        regularStar,
-        blankRegularStar,
-        blueStar,
-        blankBlueStar,
-        transcendenceStars,
-        perpetuityRingIcon,
-        eventsBackgroundTop,
-        eventsBackgroundMiddle,
-        eventsBackgroundBottom,
-        upcomingEventsText,
-        fireAdvantage,
-        waterAdvantage,
-        earthAdvantage,
-        windAdvantage,
-        lightAdvantage,
-        darkAdvantage,
-        sparkBGMask,
-        clearSparkBG,
-        clearMBSparkBG,
-        defaultSparkBG,
-        defaultMBSparkBG,
-        developerTitle,
-        VIPTitle,
-        progressBars,
-        skydomWallpaper,
-    ] = await Promise.all([
-        loadImage('assets/Player Template.png'),
-        loadImage('assets/Support Summons Template.png'),
-        loadImage('assets/Private Support Summon.png'),
-        loadImage('assets/Open Support Summon.png'),
-        loadImage('assets/Uncap Star.png'),
-        loadImage('assets/Uncap Star Blank.png'),
-        loadImage('assets/Blue Uncap Star.png'),
-        loadImage('assets/Blue Uncap Star Blank.png'),
-        Promise.all([
-            loadImage('assets/Transcendence Star 0.png'),
-            loadImage('assets/Transcendence Star 1.png'),
-            loadImage('assets/Transcendence Star 2.png'),
-            loadImage('assets/Transcendence Star 3.png'),
-            loadImage('assets/Transcendence Star 4.png'),
-            loadImage('assets/Transcendence Star 5.png'),
-            loadImage('assets/Transcendence Star Blank.png'),
-        ]),
-        loadImage('assets/Perpetuity Ring Icon.png'),
-        loadImage('assets/Events Background Top.png'),    
-        loadImage('assets/Events Background Middle.png'), 
-        loadImage('assets/Events Background Bottom.png'), 
-        loadImage('assets/Upcoming Events Text.png'),
-        loadImage('assets/Fire Advantage.png'),
-        loadImage('assets/Water Advantage.png'),
-        loadImage('assets/Earth Advantage.png'),
-        loadImage('assets/Wind Advantage.png'),
-        loadImage('assets/Light Advantage.png'),
-        loadImage('assets/Dark Advantage.png'),
-        loadImage('assets/Spark Template Background Mask.png'),
-        loadImage('assets/Spark Template Translucent BG.png'),
-        loadImage('assets/Spark Template Translucent BG with MobaCoin.png'),
-        loadImage('assets/Spark Template.png'),
-        loadImage('assets/Spark Template with MobaCoin.png'),
-        loadImage('assets/Developer Title.png'),
-        loadImage('assets/VIP Title.png'),
-        Promise.all([
-            loadImage('assets/Progress Bar 1.png'),
-            loadImage('assets/Progress Bar 2.png'),
-            loadImage('assets/Progress Bar 3.png'),
-            loadImage('assets/Progress Bar 4.png'),
-            loadImage('assets/Progress Bar 5.png'),
-            loadImage('assets/Progress Bar 6.png'),
-        ]),
-        loadImage('assets/Skydom Wallpaper.png')
-    ])
+    const assetPaths = readdirSync('assets/')
+    const loadedAssets = await Promise.all(assetPaths.map(asset => loadImage(`assets/${asset}`)))
+    const assets = Object.fromEntries(loadedAssets.map((asset, i) => [String(assetPaths[i].match(/[^.]+/)), asset]));
+
+    playerTemplate = assets['Player Template']
+    summonsTemplate = assets['Support Summons Template']
+    privateSummon = assets['Private Support Summon']
+    openSummon = assets['Open Support Summon']
+    regularStar = assets['Uncap Star']
+    blankRegularStar = assets['Uncap Star Blank']
+    blueStar = assets['Blue Uncap Star']
+    blankBlueStar = assets['Blue Uncap Star Blank']
+    transcendenceStars = [
+        assets['Transcendence Star 0'],
+        assets['Transcendence Star 1'],
+        assets['Transcendence Star 2'],
+        assets['Transcendence Star 3'],
+        assets['Transcendence Star 4'],
+        assets['Transcendence Star 5'],
+        assets['Transcendence Star Blank']
+    ]
+    perpetuityRingIcon = assets['Perpetuity Ring Icon']
+    eventsBackgroundTop = assets['Events Background Top']
+    eventsBackgroundMiddle = assets['Events Background Middle']
+    eventsBackgroundBottom = assets['Events Background Bottom']
+    upcomingEventsText = assets['Upcoming Events Text']
+    fireAdvantage = assets['Fire Advantage']
+    waterAdvantage = assets['Water Advantage']
+    earthAdvantage = assets['Earth Advantage']
+    windAdvantage = assets['Wind Advantage']
+    lightAdvantage = assets['Light Advantage']
+    darkAdvantage = assets['Dark Advantage']
+    sparkBGMask = assets['Spark Template Background Mask']
+    clearSparkBG = assets['Spark Template Translucent BG']
+    clearMBSparkBG = assets['Spark Template Translucent BG with MobaCoin']
+    defaultSparkBG = assets['Spark Template']
+    defaultMBSparkBG = assets['Spark Template with MobaCoin']
+    developerTitle = assets['Developer Title']
+    VIPTitle = assets['VIP Title']
+    progressBars = [
+        assets['Progress Bar 1'],
+        assets['Progress Bar 2'],
+        assets['Progress Bar 3'],
+        assets['Progress Bar 4'],
+        assets['Progress Bar 5'],
+        assets['Progress Bar 6'],
+    ]
+    skydomWallpaper = assets['Skydom Wallpaper']
+    
     console.log(`Assets loaded for Shard #${currentShardID}`)
 }
