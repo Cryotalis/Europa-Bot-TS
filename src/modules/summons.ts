ec9dcf8d72cf5caace3e89a3e18af882cb225a70
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-12-31 21:15:36 -0800

    Updating deprecated code

--- src/modules/summons.ts
+++ src/modules/summons.ts
@@ -13,7 +13,7 @@ export async function loadSummons(interaction: ChatInputCommandInteraction, play
     interaction.editReply({embeds: [playerEmbed], components: []})
 
     // Access the Player profile page
-    if (!browser.isConnected()) await startPuppeteer()
+    if (!browser?.connected) await startPuppeteer()
     const page = await browser.newPage()
     await page.setCookie(languageCookie, accessCookie)
     await page.goto(`http://game.granbluefantasy.jp/#profile/${playerID}`, { waitUntil: 'networkidle0' })
