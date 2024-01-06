53e381456267cc7231aaa1e7f3b156d8fecc233c
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-01-06 11:52:42 -0800

    Fixing the link for the command list

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -9,7 +9,7 @@ module.exports = {
 		const helpEmbed = new EmbedBuilder()
 			.setColor('Blue')
 			.setTitle('About Europa')
-			.setDescription('The complete list of commands for Europa can be found [here](https://cryotalis.github.io/Europa).')
+			.setDescription('The complete list of commands for Europa can be found [here](https://cryotalis.github.io/Europa/commands.html).')
 			.setThumbnail('https://i.imgur.com/f0MfwDw.png')
 			.addFields([{name: '\u200b', value: 'If you want to talk to my creator about anything, please join the [support server](https://discord.gg/YtwzVSp).'}])
     	return interaction.reply({embeds: [helpEmbed]})
