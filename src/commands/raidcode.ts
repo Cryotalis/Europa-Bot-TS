c54eb79fcecb69a788129adcdd75da8abe882ba4
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-01-17 19:21:36 -0800

    Adding raidcode command for better raid code sharing

---
+++ src/commands/raidcode.ts
@@ -0,0 +1,46 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('raidcode')
+		.setDescription('Share a raid code')
+		.addStringOption(option => option.setName('code').setDescription('The code for the raid').setRequired(true))
+		.addRoleOption(option => option.setName('role').setDescription('The role to ping for the raid'))
+		.addStringOption(option => option.setName('name').setDescription('The name of the boss'))
+		.addStringOption(option => option.setName('message').setDescription('A message to send along with the raid code'))
+		.addIntegerOption(option => option.setName('time').setDescription('The amount of time (in minutes) remaining in the raid'))
+		.addIntegerOption(option => option.setName('hp-percent').setDescription('The percentage of hp the boss currently has. (Omit the % sign)').setMinValue(1).setMaxValue(100))
+		.addIntegerOption(option => option.setName('hp').setDescription('The amount of hp the boss currently has'))
+		.addStringOption(option => option.setName('participants').setDescription('The number of participants / max participants for the raid. (Ex: 1/6)'))
+		.addStringOption(option => option.setName('raid-id').setDescription('The url to join the raid'))
+		.addStringOption(option => option.setName('quest-id').setDescription('The quest id for the raid'))
+	,
+	async execute(interaction: ChatInputCommandInteraction) {
+		const code = interaction.options.getString('code')!
+		const role = interaction.options.getRole('role')
+		const name = interaction.options.getString('name')
+		const message = interaction.options.getString('message')
+		const time = interaction.options.getInteger('time')
+		const hpPercent = interaction.options.getInteger('hp-percent')
+		const hp = interaction.options.getInteger('hp')?.toLocaleString()
+		const participants = interaction.options.getString('participants')
+		const raidID = interaction.options.getString('raid-id')
+		const questID = interaction.options.getString('quest-id')
+
+		const raidEmbed = new EmbedBuilder()
+			.setAuthor({
+				iconURL: 'https://raw.githubusercontent.com/Cryotalis/Europa-Bot-TS/main/assets/Raid%20Icon.png',
+				name: name ?? role?.name ?? 'Unknown Raid',
+				url: questID && raidID ? `https://game.granbluefantasy.jp/#quest/supporter_raid/${raidID}/${questID}/1/2/0/6` : undefined
+			})
+			.setDescription(message)
+			.setTitle(code)
+			.setColor('Blue')
+
+		if (time) raidEmbed.addFields({name: 'Time Remaining', value: `Ends <t:${Math.trunc(new Date().getTime() / 1000 + time! * 60)}:R>`, inline: true})
+		if (hp || hpPercent) raidEmbed.addFields({name: 'Boss HP', value: (hp && hpPercent) ? `${hp} [${hpPercent}%]` : (hp ?? hpPercent + '%'), inline: true})
+		if (participants) raidEmbed.addFields({name: 'Participants', value: participants, inline: true})
+		if (questID) raidEmbed.setThumbnail(`https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img/sp/quest/assets/lobby/${questID}.png`)
+		await interaction.reply({content: role ? '## ' + String(role) : undefined, embeds: [raidEmbed], allowedMentions: {roles: role ? [role.id] : undefined}})
+	}
+}
