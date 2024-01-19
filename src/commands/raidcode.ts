c89af560c2b9b077717a10ad9095c5e7be70c3ad
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-01-18 19:03:31 -0800

    Updating raidcode command to let users know about the bookmarklet

--- src/commands/raidcode.ts
+++ src/commands/raidcode.ts
@@ -9,8 +9,8 @@ module.exports = {
 		.addStringOption(option => option.setName('name').setDescription('The name of the boss'))
 		.addStringOption(option => option.setName('message').setDescription('A message to send along with the raid code'))
 		.addIntegerOption(option => option.setName('time').setDescription('The amount of time (in minutes) remaining in the raid'))
-		.addIntegerOption(option => option.setName('hp-percent').setDescription('The percentage of hp the boss currently has. (Omit the % sign)').setMinValue(1).setMaxValue(100))
 		.addIntegerOption(option => option.setName('hp').setDescription('The amount of hp the boss currently has'))
+		.addIntegerOption(option => option.setName('hp-percent').setDescription('The percentage of hp the boss currently has. (Omit the % sign)').setMinValue(1).setMaxValue(100))
 		.addStringOption(option => option.setName('participants').setDescription('The number of participants / max participants for the raid. (Ex: 1/6)'))
 		.addStringOption(option => option.setName('quest-id').setDescription('The quest id for the raid'))
 	,
@@ -20,8 +20,8 @@ module.exports = {
 		const name = interaction.options.getString('name')
 		const message = interaction.options.getString('message')
 		const time = interaction.options.getInteger('time')
-		const hpPercent = interaction.options.getInteger('hp-percent')
 		const hp = interaction.options.getInteger('hp')?.toLocaleString()
+		const hpPercent = interaction.options.getInteger('hp-percent')
 		const participants = interaction.options.getString('participants')
 		const questID = interaction.options.getString('quest-id')
 
@@ -34,10 +34,11 @@ module.exports = {
 			.setTitle(code)
 			.setColor('Blue')
 
-		if (time) raidEmbed.addFields({name: 'Time Remaining', value: `Ends <t:${Math.trunc(new Date().getTime() / 1000 + time! * 60)}:R>`, inline: true})
+		if (time) 			 raidEmbed.addFields({name: 'Time Remaining', value: `Ends <t:${Math.trunc(new Date().getTime() / 1000 + time! * 60)}:R>`, inline: true})
 		if (hp || hpPercent) raidEmbed.addFields({name: 'Boss HP', value: (hp && hpPercent) ? `${hp} [${hpPercent}%]` : (hp ?? hpPercent + '%'), inline: true})
-		if (participants) raidEmbed.addFields({name: 'Participants', value: participants, inline: true})
-		if (questID) raidEmbed.setThumbnail(`https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img/sp/quest/assets/lobby/${questID}.png`)
+		if (participants) 	 raidEmbed.addFields({name: 'Participants', value: participants, inline: true})
+		if (questID) 		 raidEmbed.setThumbnail(`https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img/sp/quest/assets/lobby/${questID}.png`)
+		if (!(name && time && hp && hpPercent && participants && questID)) raidEmbed.addFields({name: '\u200B', value: '❗ For best results, use the associated [bookmarklet](https://cryotalis.github.io/Demo/bookmarklets.html).'})
 		await interaction.reply({content: role ? '## ' + String(role) : undefined, embeds: [raidEmbed], allowedMentions: {roles: role ? [role.id] : undefined}})
 	}
 }
