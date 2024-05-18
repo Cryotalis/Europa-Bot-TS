fed5c11ac890b461e707df55c2a171023e369466
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-05-18 01:04:42 -0700

    Temporarily commenting out autocomplete function Accidentally committed the code before finishing the raid roles system

--- src/events/interaction.ts
+++ src/events/interaction.ts
@@ -1,6 +1,6 @@
 import { AutocompleteInteraction, CacheType, ChatInputCommandInteraction, Client, MessageContextMenuCommandInteraction, TextChannel, UserContextMenuCommandInteraction } from "discord.js"
 import { findBestCIMatch } from "../modules/string"
-import { raids } from "../data/raids"
+// import { raids } from "../data/raids"
 import { inspect } from "util"
 import { client, errorChannelID, homeServerShardID, logChannelID, modCommands } from "../bot"
 
@@ -8,20 +8,20 @@ import { client, errorChannelID, homeServerShardID, logChannelID, modCommands } 
  * Handles Autocomplete interactions
  */
 export function handleAutocomplete(interaction: AutocompleteInteraction<CacheType>) {
-    switch (interaction.commandName) {
-        case 'rolelist':
-            const focusedOption = interaction.options.getFocused(true)
-            if (focusedOption.name !== 'raid') return
+    // switch (interaction.commandName) {
+    //     case 'rolelist':
+    //         const focusedOption = interaction.options.getFocused(true)
+    //         if (focusedOption.name !== 'raid') return
             
-            const ratedChoices = findBestCIMatch(focusedOption.value, raids.map(({name}) => name)).ratings
-            const bestChoices = ratedChoices.sort((a, b) => b.rating - a.rating)
-            const results = bestChoices.slice(0, 10).map(({target}) => ({
-                name: target, 
-                value: raids.find(({name}) => name === target)!.value
-            }))
+    //         const ratedChoices = findBestCIMatch(focusedOption.value, raids.map(({name}) => name)).ratings
+    //         const bestChoices = ratedChoices.sort((a, b) => b.rating - a.rating)
+    //         const results = bestChoices.slice(0, 10).map(({target}) => ({
+    //             name: target, 
+    //             value: raids.find(({name}) => name === target)!.value
+    //         }))
     
-            return interaction.respond(results).catch(() => {})
-    }
+    //         return interaction.respond(results).catch(() => {})
+    // }
 }
 
 /*
