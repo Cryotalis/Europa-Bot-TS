1047090266c7481fab585876fd4aff030e544596
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-05-20 00:05:33 -0700

    Adding raid role auto-ping functionality

--- src/commands/raidcode.ts
+++ src/commands/raidcode.ts
@@ -1,4 +1,5 @@
 import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { categoryRole, servers } from '../bot'
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,9 +16,9 @@ module.exports = {
 		.addStringOption(option => option.setName('quest-id').setDescription('The quest id for the raid'))
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
-		const code = interaction.options.getString('code')!.substring(0, 10)
-		const role = interaction.options.getRole('role')
-		const name = interaction.options.getString('name')
+		const raidCode = interaction.options.getString('code')!.substring(0, 10)
+		let raidRole = interaction.options.getRole('role')
+		let raidName = interaction.options.getString('name')
 		const message = interaction.options.getString('message')
 		const time = interaction.options.getInteger('time')
 		const hp = interaction.options.getInteger('hp')?.toLocaleString()
@@ -25,21 +26,30 @@ module.exports = {
 		const participants = interaction.options.getString('participants')
 		const questID = interaction.options.getString('quest-id')
 
+		if (raidName === 'Lvl 200 Uriel') raidName = 'Lvl 200 Four Primarchs'
+		if (raidName === 'Lvl 120 Huanglong') raidName = 'Lvl 120 Huanglong & Qilin'
+
+		const server = servers.find(server => server.get('guildID') === interaction.guildId)
+		const roles: categoryRole[] = JSON.parse(server?.get('roles'))
+		const autoRaidRole = roles.find(role => role.raid === raidName)
+		if (!raidRole && autoRaidRole) raidRole = await interaction.guild?.roles.fetch(autoRaidRole.id) ?? null
+
 		const raidEmbed = new EmbedBuilder()
 			.setAuthor({
 				iconURL: 'https://raw.githubusercontent.com/Cryotalis/Europa-Bot-TS/main/assets/Raid%20Icon.png',
-				name: name ?? role?.name ?? 'Unknown Raid',
+				name: raidName ?? raidRole?.name ?? 'Unknown Raid',
 			})
 			.setDescription(message)
-			.setTitle(code)
+			.setTitle(raidCode)
 			.setColor('Blue')
 
 		if (time) 			 raidEmbed.addFields({name: 'Time Remaining', value: `Ends <t:${Math.trunc(new Date().getTime() / 1000 + time! * 60)}:R>`, inline: true})
 		if (hp || hpPercent) raidEmbed.addFields({name: 'Boss HP', value: (hp && hpPercent) ? `${hp} [${hpPercent}%]` : (hp ?? hpPercent + '%'), inline: true})
 		if (participants) 	 raidEmbed.addFields({name: 'Participants', value: participants, inline: true})
 		if (questID) 		 raidEmbed.setThumbnail(`https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img/sp/quest/assets/lobby/${questID}.png`)
-		if (!(name && time && hp && hpPercent && participants && questID)) raidEmbed.addFields({name: '\u200B', value: '❗ For best results, use the associated [bookmarklet](https://cryotalis.github.io/Demo/bookmarklets.html).'})
-		await interaction.reply({content: role ? '### ' + String(role) : undefined, embeds: [raidEmbed], allowedMentions: {roles: role ? [role.id] : undefined}})
-		await interaction.followUp(code)
+		if (!(raidName && time && hp && hpPercent && participants && questID)) raidEmbed.addFields({name: '\u200B', value: '❗ For best results, use the associated [bookmarklet](https://cryotalis.github.io/Demo/bookmarklets.html).'})
+		
+		await interaction.reply({content: raidRole ? '### ' + String(raidRole) : undefined, embeds: [raidEmbed], allowedMentions: {roles: raidRole ? [raidRole.id] : undefined}})
+		await interaction.followUp(raidCode)
 	}
 }
