081c836aff39124d0d6d16f754db2904a2043af6
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-05-20 00:01:55 -0700

    Adding raid data to be used with the raidcode command

---
+++ src/data/raids.ts
@@ -0,0 +1,73 @@
+export const raids = [
+    {"name": "Lucilius Zero",            "value": "Lvl 325 Lucilius"},
+    {"name": "Faa Zero",                 "value": "Lvl 325 Lucilius"},
+    {"name": "Hexachromatic Hierarch",   "value": "Lvl 325 Hexachromatic Hierarch"},
+    {"name": "Super Ultimate Bahamut",   "value": "Lvl 300 Super Ultimate Bahamut"},
+    
+    {"name": "Agastia",                  "value": "Lvl 275 Agastia"},
+    {"name": "Cosmos",                   "value": "Lvl 275 Cosmos"},
+    {"name": "Seofon",                   "value": "Lvl 275 Seofon"},
+    {"name": "Siegfried",                "value": "Lvl 275 Siegfried"},
+    {"name": "Diaspora",                 "value": "Lvl 275 Diaspora"},
+    {"name": "Mugen",                    "value": "Lvl 275 Mugen"},
+
+    {"name": "The World",                "value": "Lvl 250 The World"},
+    {"name": "Belial",                   "value": "Lvl 250 Belial"},
+    {"name": "Beelzebub",                "value": "Lvl 250 Beelzebub"},
+    {"name": "Lucilius HL",              "value": "Lvl 250 Lucilius"},
+    {"name": "Faa HL",                   "value": "Lvl 250 Lucilius"},
+
+    {"name": "Ultimate Bahamut HL",      "value": "Lvl 200 Ultimate Bahamut"},
+    {"name": "Grand Order HL",           "value": "Lvl 200 Grand Order"},
+    {"name": "Akasha",                   "value": "Lvl 200 Akasha"},
+    {"name": "Proto Bahamut HL",         "value": "Lvl 150 Proto Bahamut"},
+
+    {"name": "Wilnas",                   "value": "Lvl 200 Wilnas"},
+    {"name": "Wamdus",                   "value": "Lvl 200 Wamdus"},
+    {"name": "Galleon",                  "value": "Lvl 200 Galleon"},
+    {"name": "Ewiyar",                   "value": "Lvl 200 Ewiyar"},
+    {"name": "Lu Woh",                   "value": "Lvl 200 Lu Woh"},
+    {"name": "Fediel",                   "value": "Lvl 200 Fediel"},
+
+    {"name": "Tiamat Aura Omega",        "value": "Lvl 200 Tiamat Aura Omega"},
+    {"name": "Colossus Ira Omega",       "value": "Lvl 200 Colossus Ira Omega"},
+    // {"name": "Leviathan ??? Omega",      "value": "Lvl 200 Leviathan ??? Omega"},
+    // {"name": "Yggdrasil ??? Omega",      "value": "Lvl 200 Yggdrasil ??? Omega"},
+    {"name": "Luminiera Credo Omega",    "value": "Lvl 200 Luminiera Credo Omega"},
+    {"name": "Celeste Ater Omega",       "value": "Lvl 200 Celeste Ater Omega"},
+    
+    {"name": "Huanglong & Qilin",        "value": "Lvl 120 Huanglong & Qilin"},     // This is a custom name. The in-game twitter raid name is "Lvl 120 Huanglong"
+    {"name": "Lucilius",                 "value": "Lvl 150 Lucilius"},
+    {"name": "Faa",                      "value": "Lvl 150 Lucilius"},
+    {"name": "Four Primarchs",           "value": "Lvl 200 Four Primarchs"},        // This is a custom name. The in-game twitter raid name is "Lvl 200 Uriel"
+    {"name": "Lindwurm",                 "value": "Lvl 200 Lindwurm"},
+
+    {"name": "Tiamat Malice",            "value": "Lvl 150 Tiamat Malice"},
+    {"name": "Leviathan Malice",         "value": "Lvl 150 Leviathan Malice"},
+    {"name": "Phronesis",                "value": "Lvl 150 Phronesis"},
+    {"name": "Luminiera Malice",         "value": "Lvl 150 Luminiera Malice"},
+    {"name": "Anima-Animus Core",        "value": "Lvl 150 Anima-Animus Core"},
+    {"name": "Legion Void",              "value": "Lvl 150 Legion Void"},
+
+    {"name": "Atum",                     "value": "Lvl 120 Atum"},
+    {"name": "Tefnut",                   "value": "Lvl 120 Tefnut"},
+    {"name": "Bennu",                    "value": "Lvl 120 Bennu"},
+    {"name": "Ra",                       "value": "Lvl 120 Ra"},
+    {"name": "Horus",                    "value": "Lvl 120 Horus"},
+    {"name": "Osiris",                   "value": "Lvl 120 Osiris"},
+    
+    {"name": "Shiva",                    "value": "Lvl 120 Shiva"},
+    {"name": "Europa",                   "value": "Lvl 120 Europa"},
+    {"name": "Godsworn Alexiel",         "value": "Lvl 120 Godsworn Alexiel"},
+    {"name": "Grimnir",                  "value": "Lvl 120 Grimnir"},
+    {"name": "Metatron",                 "value": "Lvl 120 Metatron"},
+    {"name": "Avatar",                   "value": "Lvl 120 Avatar"},
+    {"name": "Rose Queen",               "value": "Lvl 110 Rose Queen"},
+
+    {"name": "Tiamat Omega",             "value": "Lvl 100 Tiamat Omega Ayr"},
+    {"name": "Colossus Omega",           "value": "Lvl 100 Colossus Omega"},
+    {"name": "Leviathan Omega",          "value": "Lvl 100 Leviathan Omega"},
+    {"name": "Yggdrasil Omega",          "value": "Lvl 100 Yggdrasil Omega"},
+    {"name": "Luminiera Omega",          "value": "Lvl 100 Luminiera Omega"},
+    {"name": "Celeste Omega",            "value": "Lvl 100 Celeste Omega"}
+]
