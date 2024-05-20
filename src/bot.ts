527ca15f534bd9e1da0ce83789d49ac41aa3888b
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-05-19 23:58:29 -0700

    Adding a raid property to categoryRole for use with the raidcode command

--- src/bot.ts
+++ src/bot.ts
@@ -57,13 +57,18 @@ export async function registerCommands() {
 		.catch(console.error)
 }
 
-export interface categoryRole {id: string, category: string}
+/**
+ * - If category is defined, the role can be assigned by members.
+ * - If raid is defined, the role is tied to a raid for use with /raidcode.
+ * - At least one of category or raid should always be defined.
+ */
+export interface categoryRole {id: string, category?: string, raid?: string}
 export interface serverData {guildName: string, guildID: string, greeting: string, roles: string, events: string}
 export interface userData {
 	username: string,	userID: string,
 	crystals: string,	mobaCoin: string,
-	tickets: string,    tenParts: string, 	
-	rolls: string, 		background: string,	
+	tickets: string,    tenParts: string,
+	rolls: string, 		background: string,
 	sparkTitle: string
 }
 export let publicDB: GoogleSpreadsheet
