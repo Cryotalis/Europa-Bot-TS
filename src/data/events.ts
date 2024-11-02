fad29aa3dbde7c66aa47893504af09c01924a654
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-11-02 14:09:19 -0700

    Creating a data file for storing event related data

---
+++ src/data/events.ts
@@ -0,0 +1,29 @@
+export interface relayEvent {name: string, roleID: number, channelID: number, time: number}
+
+export const recurringEvents = [
+    "Unite and Fight",
+    "Dread Barrage",
+    "Rise of the Beasts",
+    "Tower of Babyl",
+    "Proving Grounds",
+    
+    "Tales of Arcarum",
+    "Tales of Arcarum: Justice",
+    "Tales of Arcarum: The Hanged Man",
+    "Tales of Arcarum: Death",
+    "Tales of Arcarum: Temperance",
+    "Tales of Arcarum: The Devil",
+    "Tales of Arcarum: The Tower",
+    "Tales of Arcarum: The Star",
+    "Tales of Arcarum: The Moon",
+    "Tales of Arcarum: The Sun",
+    "Tales of Arcarum: Judgement",
+
+    "Exo Crucible",
+    "Exo Sagittarius Crucible",
+    "Exo Vohu Manah Crucible",
+    "Exo Cocytus Crucible Crucible",
+    "Exo Ifrit Crucible",
+    "Exo Corow Crucible",
+    "Exo Diablo Crucible",
+]
